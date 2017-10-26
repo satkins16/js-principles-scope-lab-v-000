70def7c98481635d0b1e9fdec9f191d43007168e
Scott Atkins <satkins16@gmail.com>
2017-10-26 13:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function changeLeastFavoriteCustomer () {
 }
 
 function attemptTwoFavoriteCustomers () {
-  let favoriteCustomer = 'bar';
+  const favoriteCustomer = 'bar';
   let favoriteCustomer = 'not bar';
 }
